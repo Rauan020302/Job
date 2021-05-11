2b044050dd1f2cacd91609e65e19ec817691baff
Рауан <ibragimov020302@gmail.com>
2021-05-11 23:22:37 +0600

    commit 11.05

---
+++ src/test/java/kg/itacademy/springsecurityproject/SpringSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.itacademy.springsecurityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
